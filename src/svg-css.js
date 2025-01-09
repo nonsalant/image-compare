ef7ede740032d4c1a696c2c84705eaeeb392cfa8
Stefan Matei <nonsalant@gmail.com>
2025-01-09 23:29:47 +0200

    clean up svg-css.js

--- src/svg-css.js
+++ src/svg-css.js
@@ -6,13 +6,15 @@ export function svgCss(
     strokeWidth = '4',
     color = 'hsla(0, 0%, 0%, 1)' // any format except hex
 ) {
-    const commonAtts = 'stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"';
-    const arrowLeftStroke = `"${strokeWidth}" style="stroke: ${color};" ${commonAtts}`;
-    const arrowRightStroke = `"${strokeWidth}" style="stroke: ${color};" ${commonAtts}`;
-    const dataUrl = `data:image/svg+xml;utf8,`
-        + `<svg viewbox="0 0 60 60" width="60" height="60" xmlns="http://www.w3.org/2000/svg">`
-        + `<path stroke-width=${arrowLeftStroke}  fill="none" d="M20 20 L10 30 L20 40"/>`
-        + `<path stroke-width=${arrowRightStroke} fill="none" d="M40 20 L50 30 L40 40"/>`
-        + `</svg>`;
+    const atts = `stroke-width="${strokeWidth}" style="stroke: ${color};" `
+        + 'stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" fill="none"';
+
+    // this should be a single line, hence the concatenation
+    const dataUrl = 'data:image/svg+xml;utf8,'
+        + '<svg viewbox="0 0 60 60" width="60" height="60" xmlns="http://www.w3.org/2000/svg">'
+            + `<path ${atts} d="M20 20 L10 30 L20 40"/>`
+            + `<path ${atts} d="M40 20 L50 30 L40 40"/>`
+        + '</svg>';
+    
     return `image-compare { --thumb-background-image: url('${dataUrl}'); }`;
 }
